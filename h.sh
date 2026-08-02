3f88bcfd1dfa0f8a5a815340775d2fa6e6c1b8b0
Moino_bann <123836435+hamzaabde-langjk@users.noreply.github.com>
2026-08-02 16:55:39 +0100

    Enhance h.sh with user guidance comments
    Added comments for user guidance on terminal usage and customization.

--- h.sh
+++ h.sh
@@ -10,3 +10,7 @@ if [ "$1" = "hm" ]; then
 	  echo "user fld change bar size"
     
 fi
+#enter in the terminal hm or hmz for see the change enjoy !
+#you can choice what u whent like your name  and make sure you edit ~/.config/wezterm.lua by your local picture
+#the format of picture that u can use it is png jpg and gif also  you cant choice video cuase it crash the terminal 
+# any problem inbox me my IG: fake ego 
